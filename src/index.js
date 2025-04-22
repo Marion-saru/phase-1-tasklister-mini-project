7cdbf56a7270749d6af43727a45c0d70cd2590bf
Marion Saru <maghangasaru@gmail.com>
2025-04-22 15:10:37 +0300

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+  let li;
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const input = document.getElementById("new-task-description");
+    const taskText = input.value;
+
+    if (taskText.trim() !== "") {
+      li = document.createElement("li");
+      li.textContent = taskText;
+      taskList.appendChild(li);
+      input.value = ""; 
+    }
+
+    // delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "delete";
+    deleteBtn.style.marginLeft = "10px";
+    deleteBtn.style.fontSize ="10px"
+
+    deleteBtn.addEventListener("click", () => {
+      li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    input.value = ""; 
+
+  });
 });
